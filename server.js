7ae4202ce8d891c313638aa69b5656c9b528378d
Om Shiv Verma <omshivpatel111@gmail.com>
2023-08-24 16:02:01 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,34 +1,64 @@
-const express = require('express')
-const ejs = require('ejs')
-const expresslayout = require('express-ejs-layouts')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const expresslayout = require("express-ejs-layouts");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoStore = require("connect-mongo");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+//database connection
+const url = "mongodb://0.0.0.0:27017/pizza";
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+mongoose.connection
+  .once("open", () => {
+    console.log("Database connected.......");
+  })
+  .on("error", (err) => {
+    console.log("connection failed....");
+  });
+
+// session store
+let mongoStore = new MongoStore({
+  mongoUrl: url,
+  collection: 'sessions'
+})
+
+// Session config
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }//24 hours
+  })
+);
+
+app.use(flash());
 
 //assets
-app.use(express.static('public'))
-
+app.use(express.static("public"));
+app.use(express.json())
+// Global middleware
+app.use((req, res, next)=>{
+  res.locals.session = req.session
+  next()
+})
 //set Template engine
-app.use(expresslayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine','ejs')
+app.use(expresslayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-
-app.get('/', (req, res) =>{
-    res.render('home')
-})
-app.get('/cart', (req, res) =>{
-    res.render('customers/cart')
-})
-app.get('/login', (req, res) =>{
-    res.render('auth/login')
-})
-app.get('/register', (req, res) =>{
-    res.render('auth/register')
-})
-
+//routes
+require("./routes/web")(app);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port  ${PORT}`);
-})
+  console.log(`Listening on port  ${PORT}`);
+});
